8856e8f0f4405610fd777dedb8641ac95a647507
jerabelo <70666626+jerabelo@users.noreply.github.com>
2022-02-17 22:33:37 -0700

    input.php

---
+++ input.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Grocery Shopping System</title>
+    </head>
+    <body>
+        <h1>Food List</h1>
+        <form action="list.php", method = post>
+            <input type="text" name = "foodname">
+            <br>
+            <br>
+            <input type="text" name = "plu">
+            <br>
+            <br>
+            <input type="submit" value="submit">            
+        </form>
+    </body>
+</html>
