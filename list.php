c28c179f793e2c485472f511bff072a1d7338d8a
jerabelo <70666626+jerabelo@users.noreply.github.com>
2022-02-17 22:34:07 -0700

    list.php

---
+++ list.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Grocery Shopping System</title>
+    </head>
+    <body>
+        <h1>Active Food Items List</h1>
+        <!-- PHP Calculations start here -->
+        <?php
+        $num = $_POST['foodname'];
+        $plu = $_POST['plu'];
+        ?>
+    </body>
+</html>
